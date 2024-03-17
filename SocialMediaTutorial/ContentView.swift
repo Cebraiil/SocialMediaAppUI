f5c0664d4d89d66349e153277bdc471ac6b5d196
cebrailblox <eymencebrail@icloud.com>
2024-03-17 15:40:45 +0300

    Initial Commit

---
+++ SocialMediaTutorial/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SocialMediaTutorial
+//
+//  Created by Eymen on 14.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabbarView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
